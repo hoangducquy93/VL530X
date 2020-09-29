8b985a3740b7d1e725ea2595f0ac30f2e36c10d4
D.Q.Hoang <54616267+hoangducquy93@users.noreply.github.com>
2020-09-29 11:14:42 +0700

    Add files via upload

---
+++ Debug/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.d
@@ -0,0 +1,131 @@
+Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.o: \
+ ../Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.c \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/USB_HOST/Target/usbh_conf.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Core/Inc/main.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Core/Inc/stm32f4xx_hal_conf.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Include/core_cm4.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Include/cmsis_version.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Include/cmsis_compiler.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Include/mpu_armv7.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_def.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ioreq.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_pipes.h \
+ D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/USB_HOST/Target/usbh_conf.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Core/Inc/main.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Core/Inc/stm32f4xx_hal_conf.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Include/core_cm4.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Include/cmsis_version.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Include/mpu_armv7.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_def.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ioreq.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_pipes.h:
+
+D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h:
---
+++ Debug/USB_HOST/App/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../USB_HOST/App/usb_host.c 
+
+OBJS += \
+./USB_HOST/App/usb_host.o 
+
+C_DEPS += \
+./USB_HOST/App/usb_host.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+USB_HOST/App/%.o: ../USB_HOST/App/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DUSE_HAL_DRIVER -DSTM32F407xx -I"D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/USB_HOST/App" -I"D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/USB_HOST/Target" -I"D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Core/Inc" -I"D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc" -I"D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Middlewares/ST/STM32_USB_Host_Library/Core/Inc" -I"D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc" -I"D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"D:/Project/workspace/VL53L0X_STM32F4_multii2c-master/VL53L0X_STM32F4_multii2c-master/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
